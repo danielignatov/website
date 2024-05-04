1ba8dbfd11c11ba81c43b416b9724aedc0ab38db
Daniel Ignatov <danielignatov@outlook.com>
2024-05-04 19:55:08 +0300

    font awesome component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 export default function Contact() {
     return (
         <div className="row d-flex justify-content-center">
             <div className="col-12 col-lg-4 d-flex justify-content-center">
                 <span className="contact-link">
-                    <i className="fas fa-map-marker-alt"></i> Sofia, Bulgaria
+                <FontAwesomeIcon icon={faMapMarkerAlt} /> Sofia, Bulgaria
                 </span>
             </div>
             <div className="col-12 col-lg-4 d-flex justify-content-center">
                 <span className="contact-link">
-                    <a href="https://github.com/danielignatov" target="_blank"><i className="fab fa-github"></i>
-                        github/danielignatov</a>
+                    <a href="https://github.com/danielignatov" target="_blank">
+                    <FontAwesomeIcon icon={faGithub} /> github/danielignatov</a>
                 </span>
             </div>
             <div className="col-12 col-lg-4 d-flex justify-content-center">
                 <span className="contact-link">
-                    <a href="https://www.linkedin.com/in/danielignatov/" target="_blank"><i className="fab fa-linkedin"></i>
-                        linkedin/danielignatov</a>
+                    <a href="https://www.linkedin.com/in/danielignatov/" target="_blank">
+                    <FontAwesomeIcon icon={faLinkedin} /> linkedin/danielignatov</a>
                 </span>
             </div>
         </div>
